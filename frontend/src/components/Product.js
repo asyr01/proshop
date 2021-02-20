68eff94ff694f7c057a6a8e59a67a078e4730a6b
example <alisayar0107@gmail.com>
2021-02-20 16:04:07 +0300

    propTypes added for checking if we are using correct datatype

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import Rating from '../components/Rating'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -19,7 +19,7 @@ const Product = ({product}) => {
           <Card.Text as='div'>
               <Rating
                value={product.rating}
-               text={`${product.numReviews}`}
+               text={`${product.numReviews} reviews`}
                />
           </Card.Text>
           
