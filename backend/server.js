68079de092c7feff20c3263cd5d95af0987fa205
example <alisayar0107@gmail.com>
2021-02-26 15:38:53 +0300

    Custom error handling added to the project

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import  {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
-
 
 dotenv.config();
 
@@ -12,21 +12,16 @@ connectDB();
 
 const app = express();
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === production ? null : err.stack
-    })
-})
-
 app.get('/', (req, res) => {
     res.send('API is running');
 })
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+
+// Custom Error Handling
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
