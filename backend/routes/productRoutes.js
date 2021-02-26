04c320090ee91d1d270d5fee3fa21bfe9c45074b
example <alisayar0107@gmail.com>
2021-02-26 13:58:01 +0300

    product routes seperated into another file for keeping app tidy

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+const router = expres.Router();
+import Product from '../models/productModel'
+
+router.get('/', async (req, res) => {
+    const products = await Product.find({})
+    res.json(products);
+})
+
+router.get('/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product);
+})
+
+export default Router;
