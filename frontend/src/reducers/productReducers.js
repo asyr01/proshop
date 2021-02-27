f49440d785a50ae700a39a9b69da243c8a95c581
example <alisayar0107@gmail.com>
2021-02-27 13:04:47 +0300

    productReducers.js created, first reducer created.

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,12 @@
+export const productListReducer = (state = { products: [] }, action) => {
+    switch(action.type) {
+      case 'PRODUCT_LIST_REQUEST':
+          return { loading: true, products: [] }
+      case 'PRODUCT_LIST_SUCCESS':
+          return {loading: true, products: action.payload }
+      case 'PRODUCLIST_FAIL':
+          return {loading: false, error: action.payload}
+      default:
+           return state
+    }
+}
