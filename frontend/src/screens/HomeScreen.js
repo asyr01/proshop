7099d16b6e6342bf2674ac3586dd2616652f2918
example <alisayar0107@gmail.com>
2021-02-28 16:00:15 +0300

    loading or error conditionals added with ternary operator

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -17,6 +17,11 @@ const HomeScreen = () => {
     return (
         <>
            <h1>Latest Products</h1>
+           { loading ? (
+             <h2>LOADING...</h2>
+           ) : error ? (
+             <h3>{error}</h3>
+           ) : ( 
            <Row>
              {products.map((product) => (
                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -24,6 +29,7 @@ const HomeScreen = () => {
                  </Col>
              ))}
            </Row>
+           )}
         </>
     )
 }
