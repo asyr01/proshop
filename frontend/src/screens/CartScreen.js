96d5874d6417c206361fdc23d2e9720511263d81
example <alisayar0107@gmail.com>
2021-03-03 15:45:33 +0300

    Needed libraries imported into CartScreen.js

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  İmage,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
   return <div>Cart</div>;
 };
 
