7888d9a85c8ade4e38a3e39ad50e8cb933c58d5b
example <alisayar0107@gmail.com>
2021-03-04 12:10:14 +0300

    userControllet.js created for authentication purposes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import  {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -12,17 +12,24 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('API is running');
-})
+  res.send('API is running');
+});
 
 app.use('/api/products', productRoutes);
 
 // Custom Error Handling
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
