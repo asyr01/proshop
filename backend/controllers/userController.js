777f4d129f028b5ed15d0f468376a51156f2d86d
example <alisayar0107@gmail.com>
2021-03-04 12:37:50 +0300

    userAuthentication provided, we are sending back users information now.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,20 @@ import User from '../models/userModel.js';
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id_: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password.');
+  }
 });
 
 export { authUser };
