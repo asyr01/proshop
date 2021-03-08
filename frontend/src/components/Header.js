8a3d4709389ffcc5298dcfefc46c663704cfa972
example <alisayar0107@gmail.com>
2021-03-08 12:05:48 +0300

    Header component arranged for making a dropdown menu and to not showing sign in when there is userInfo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
-    return (
-     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand href="/">ProShop</Navbar.Brand>
+            <Navbar.Brand href='/'>ProShop</Navbar.Brand>
           </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-             <Nav className="ml-auto">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
-                <Nav.Link><i className='fas fa-shopping-cart mr-1'></i>Cart</Nav.Link>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart mr-1'></i>Cart
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link><i className='fas fa-user mr-1'></i>Sign In</Nav.Link>
-              </LinkContainer>
-             </Nav>
-            </Navbar.Collapse>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user mr-1'></i>Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-       </Navbar>
-      </header>
-    )
-}
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
