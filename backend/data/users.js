31f5e00df934c012fa8af1a8884f28298c9fd2ce
example <alisayar0107@gmail.com>
2021-03-08 12:49:55 +0300

    RegisterScreen created.

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs';
 
 const users = [
-    {
-        name: 'Ali Sayar',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('1234564', 10),
-        isAdmin: true
-    },
-    {
-        name: 'John Doe',
-        email: 'johnd@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    },
-    {
-        name: 'Jane Doe',
-        email: 'janed@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    }
-]
+  {
+    name: 'Ali Sayar',
+    email: 'alisayar0107@gmail.com',
+    password: bcrypt.hashSync('adana0101', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'johnd@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'janed@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
 
 export default users;
