d064b8b154a479d88f35930d15794fa03f3b7279
example <alisayar0107@gmail.com>
2021-03-14 12:23:29 +0300

    isPaid and isDelivered added to the OrderScreen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -61,12 +61,27 @@ const OrderScreen = ({ match }) => {
                 {order.shippingAddress.postalCode},{' '}
                 {order.shippingAddress.country}
               </p>
+              {order.isDelivered ? (
+                <Message variant='success'>
+                  {' '}
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant='danger'>Not Delivered</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Payment Method</h2>
-              <strong>Method: </strong>
-              {order.paymentMethod}
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant='success'> Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant='danger'>Not paid</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
