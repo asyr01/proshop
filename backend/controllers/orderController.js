081b6834d299be78ef1695e5542f766b09f0f238
example <alisayar0107@gmail.com>
2021-03-16 10:01:44 +0300

    ORDER_LIST_MY constants and reducers added.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,11 +81,11 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 });
 
 // @desc  Get logged in user orders
-// @route  PUT /api/orders/myorders
+// @route  GET /api/orders/myorders
 // @access Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.findById({ user: req.user._id });
+  const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
 
