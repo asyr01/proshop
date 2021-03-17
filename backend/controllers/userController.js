8c042e2fa0d40ae4a46dfd88675a28e95ee1bea6
example <alisayar0107@gmail.com>
2021-03-17 09:44:46 +0300

    getUsers controller created for  admins to get all users data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -58,7 +58,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // @desc   Get user profile
-// @route  POST /api/users/profile
+// @route  GET /api/users/profile
 // @access Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
@@ -106,4 +106,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+// @desc   Get all users
+// @route  GET /api/users
+// @access Private/Admin
+
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
