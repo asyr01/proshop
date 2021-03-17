f388cc1ed51db23009b53c0acb6110b524f27584
example <alisayar0107@gmail.com>
2021-03-17 10:03:17 +0300

    admin authMiddleware created, and added as a second middleware addition to protect in the userRouters, to reaching getUsers

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,23 +8,30 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    console.log('token found');
-  }
-
-  try {
-    token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(401);
-    throw new Error('Not authorized, token failed.');
-  }
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized no token');
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed.');
+    }
+    if (!token) {
+      res.status(401);
+      throw new Error('Not authorized no token');
+    }
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
