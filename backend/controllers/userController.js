357f238d1e4918368867330f2a6bd2d3fe149184
example <alisayar0107@gmail.com>
2021-03-19 11:26:10 +0300

    updateUser controller created for editing admin situation, name etc

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,7 +136,39 @@ const deleteUser = asyncHandler(async (req, res) => {
 
 const getUserById = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id).select('-password');
-  res.json(user);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc   Update user
+// @route  PUT /api/users/:id
+// @access Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id_: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
 });
 
 export {
@@ -147,4 +179,5 @@ export {
   getUsers,
   deleteUser,
   getUserById,
+  updateUser,
 };
