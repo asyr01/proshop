8e5ac5abc783b56342f041939eb2d65c848c3443
example <alisayar0107@gmail.com>
2021-03-22 12:35:40 +0300

    updateProduct controller added to the productControllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -53,8 +53,48 @@ const createProduct = asyncHandler(async (req, res) => {
     description: 'Sample description',
   });
 
-  const cratedProduct = await product.save();
-  res.status(201).json(product);
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
 });
 
-export { getProducts, getProductById, deleteProduct, createProduct };
+// @desc   Update a product
+// @route  PUT /api/products/:id
+// @access Private/Admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
