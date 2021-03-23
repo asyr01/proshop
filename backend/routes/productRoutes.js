53b53995ccac1396f1287d6c247afbe7a68be44f
example <alisayar0107@gmail.com>
2021-03-23 16:17:46 +0300

    little bug with endpoint fixed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const router = express.Router();
 import {
   getProducts,
   getProductById,
@@ -7,7 +8,6 @@ import {
   deleteProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
-const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router
