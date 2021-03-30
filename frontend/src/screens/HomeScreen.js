44ab46bde1afae70f2442e8ae56c8010fe62a4f2
example <alisayar0107@gmail.com>
2021-03-30 15:44:26 +0300

    Back button added when a search made

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, Carousel } from 'react-bootstrap';
 import Product from '../components/Product';
@@ -26,7 +26,14 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-      <Meta />;{!keyword && <ProductCarousel />}
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
